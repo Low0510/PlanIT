753bc32323f58f735eb54df896e6e906b30482ba
Mac_Low <lowkaiyan14@gmail.com>
2025-11-09 01:59:09 +0800

    Remove exposed API key

--- lib/const.dart
+++
@@ -1,3 +0,0 @@
-const String GEMINI_API_KEY = 'AIzaSyBBSfqSLxpAin3_vgWM8PAFLyN8V-LibYM';
-const String OPENWEATHER_API_KEY = '9ad689f788084956f3486a07cb72522e';
-const String GOOGLE_API = 'AIzaSyDXTPLtNwtwvr9_hwZMviwSvtJF1SXkduw';
